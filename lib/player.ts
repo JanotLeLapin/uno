f762320c9fa0c33ad89d58046ee110ed1015849d
JanotLeLapin <joseph300905@gmail.com>
2020-10-22 22:27:44 +0200

    Create player class

---
+++ lib/player.ts
@@ -0,0 +1,59 @@
+import Stack from './stack';
+import Card from './card';
+
+export default class Player {
+    name: string;
+    bot: boolean;
+    stack: Stack;
+    deck: Card[];
+    constructor (name: string, bot: boolean, stack: Stack) {
+        this.name = name;
+        this.bot = bot;
+        this.stack = stack;
+        this.deck = [];
+        for (let i = 0; i < 7; i++) {
+            this.deck.push(this.stack.pickCard());
+        }
+    }
+
+    // Plays a card from the player's deck to the game deck
+    play() {
+        if (this.stack.currentCard.capacity && !this.stack.currentCard.usedCapacity) {
+            this.stack.currentCard.usedCapacity = true;
+            switch (this.stack.currentCard.capacity) {
+                case '+2':
+                    for (let i = 0; i < 2; i++) {
+                        this.deck.push(this.stack.pickCard());
+                    }
+                    console.log('-- ' + this.reprName() + ' picked 2 cards --');
+                    return;
+                case 'skip':
+                    console.log('-- ' + this.reprName() + ' turn got skipped --');
+                    return;
+                case 'reverse':
+                    console.log('-- Should reverse turns but... --');
+                    return;
+            }
+        }
+        if (this.bot) {
+            const playableCards: Card[] = [];
+            this.deck.forEach(card => {
+                if (card.color === this.stack.currentCard.color || card.number === this.stack.currentCard.number) playableCards.push(card);
+            });
+            if (!playableCards.length) {
+                const card = this.stack.pickCard();
+                this.deck.push(card);
+                console.log('-- ' + this.reprName() + ' draws ' + card.repr() + ' --');
+                return;
+            }
+            const card = playableCards[0];
+            this.deck.splice(this.deck.indexOf(card), 1);
+            this.stack.currentCard = card;
+            console.log('-- ' + this.reprName() + ' plays ' + card.repr() + ' --');
+        }
+        if (!this.deck.length) console.log('-- ' + this.reprName() + ' WINS --');
+    }
+    reprName() {
+        return this.name + (this.bot ? ' [BOT]' : '');
+    }
+}
